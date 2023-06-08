c7cad08ca4d60b08e18cb2469c05bc5e1dfb4e9d
NxtPerfect <alakaxan@protonmail.com>
2023-06-08 17:35:57 +0200

    Finished chat app, run server.py with corresponding ip and port, then client.py can connect to that

--- client.py
+++ client.py
@@ -1,18 +1,38 @@
 import socket
 import sys
+import select
+
+from _thread import *
 
 
 def run_client():
-    IP_ADDRESS = sys.argv[1]
+    IP_ADDRESS = str(sys.argv[1])
     PORT = int(sys.argv[2])
-    # bytes(sys.argv[3])
 
-    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-        # send message
-        s.connect((IP_ADDRESS, PORT))
-        s.sendall(b"msg")
-        data = s.recv(1024)
-    print(f'Message {data!r}')
+    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    if len(sys.argv) < 3:
+        print("Not enough arguments, pass in script, ip address, port")
+        exit()
+    server.connect((IP_ADDRESS, PORT))
+
+    sockets_list = [sys.stdin, server]
+
+    read_sockets, write_sockets, error_sockets = select.select(
+        sockets_list, [], [])
+
+    while True:
+        for socks in read_sockets:
+            if socks == server:
+                message = server.recv(2048)
+                print(message)
+            else:
+                msg = bytes(sys.stdin.readline(), 'utf-8')
+                server.send(msg)
+                sys.stdout.write("< You >: ")
+                sys.stdout.write(str(msg).strip("b\'\"\\n\""))
+                sys.stdout.write("\n")
+                sys.stdout.flush()
+    server.close()
 
 
 if __name__ == "__main__":
--- server.py
+++ server.py
@@ -1,6 +1,8 @@
 import socket
 import sys
 
+from _thread import *
+
 """Create a Socket:
 
     Create a socket using the socket.socket() function, specifying the socket type and address family (typically socket.AF_INET for IPv4).
@@ -31,28 +33,70 @@ Clean Up:
 
     Properly close all the sockets and release any acquired resources when the chat room is being shut down."""
 
+list_of_clients = []
+
+
+def client_thread(conn, addr):
+    print('Welcome to py-chat!')
+
+    while True:
+        try:
+            message = conn.recv(2048)
+            if message:
+                print(f'< {addr[0]} >: {message}')
+                message_to_be_sent = f'< {addr[0]} >: {message}'
+                broadcast(message_to_be_sent, conn)
+            else:
+                remove(conn)
+        except:
+            continue
+
+
+def broadcast(msg, conn):
+    for clients in list_of_clients:
+        if clients != conn:
+            try:
+                clients.send(msg)
+            except:
+                clients.close()
+                remove(clients)
+
+
+def remove(conn):
+    for connection in list_of_clients:
+        if conn == connection:
+            print('< User > disconnected')
+            message_to_be_sent = '< User > disconnected'
+            broadcast(message_to_be_sent, conn)
+            list_of_clients.remove(connection)
+
 
 def run_server():
     if len(sys.argv) < 3:
         print("Not enough arguments, pass in script, ip address, port")
         exit()
 
-    IP_ADDRESS = sys.argv[1]
+    IP_ADDRESS = str(sys.argv[1])
     PORT = int(sys.argv[2])
 
     print(f'{IP_ADDRESS}, {PORT}')
 
-    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-        s.bind((IP_ADDRESS, PORT))
-        s.listen(5)
-        clientsocket, address = s.accept()
-        with clientsocket:
-            print(f'Success! Connected to {address}')
-            while True:
-                msg = clientsocket.recv(1024)
-                if not msg:
-                    break
-                clientsocket.sendall(msg)
+    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    server.bind((IP_ADDRESS, PORT))
+    server.listen(100)
+
+    while True:
+        conn, addr = server.accept()
+
+        list_of_clients.append(conn)
+
+        print(addr[0] + " connected")
+
+        start_new_thread(client_thread, (conn, addr))
+
+    conn.close()
+    server.close()
 
 
 if __name__ == "__main__":
